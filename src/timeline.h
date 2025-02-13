e42800f9541382bd34a70420b86884771ab23907
Robbie <robbiemaldrich@gmail.com>
2025-02-13 14:59:53 -0600

    WIP revamp to tl solve algo

--- src/timeline.h
+++ src/timeline.h
@@ -23,10 +23,6 @@ typedef struct {
 } tl_OpBaseGeometry;
 
 typedef struct {
-    tl_Op* sketch;
-} tl_OpSketchGeometry;
-
-typedef struct {
     geo_Align orbitOrigin;
     HMM_Vec2 orbitAngle;
     float orbitDist;
@@ -44,10 +40,15 @@ tl_Scene tl_sceneInit() {
     return out;
 }
 
+typedef enum {
+    TL_OPS_INVALIDATED,
+    TL_OPS_SOLVED,
+    TL_OPS_MARKED_FOR_DELETE,
+} tl_OpStatus;
+
 struct tl_Op {
     tl_Op* next;
-    bool markedForDeletion;
-    bool solved;
+    tl_OpStatus status;
 
     struct {
         HMM_Vec2 pos;
@@ -59,8 +60,8 @@ struct tl_Op {
     union {
         tl_OpSketch sketch;
         tl_OpBaseGeometry baseGeometry;
-        tl_OpSketchGeometry sketchGeometry;
     } val;
+    tl_Op* dependencies[1];
 
     tl_Scene scene;
 };
@@ -70,12 +71,16 @@ typedef struct {
     tl_Op* activeOp;
     HMM_Vec2 camPos;
     float camHeight;
-    snz_Arena* arena;
+    snz_Arena* operationArena;
+    snz_Arena* generatedArena;
+    PoolAlloc* generatedPool;
 } tl_Timeline;
 
-tl_Timeline tl_timelineInit(snz_Arena* arena) {
+tl_Timeline tl_timelineInit(snz_Arena* opArena, snz_Arena* generatedArena, PoolAlloc* generatedPool) {
     tl_Timeline out = {
-        .arena = arena,
+        .operationArena = opArena,
+        .generatedArena = generatedArena,
+        .generatedPool = generatedPool,
         .camHeight = 1000,
         .camPos = HMM_V2(0, 0),
     };
@@ -83,7 +88,7 @@ tl_Timeline tl_timelineInit(snz_Arena* arena) {
 }
 
 tl_Op* tl_timelinePushSketch(tl_Timeline* tl, HMM_Vec2 pos, sk_Sketch sketch) {
-    tl_Op* out = SNZ_ARENA_PUSH(tl->arena, tl_Op);
+    tl_Op* out = SNZ_ARENA_PUSH(tl->operationArena, tl_Op);
     *out = (tl_Op){
         .ui.pos = pos,
         .kind = TL_OPK_SKETCH,
@@ -96,7 +101,7 @@ tl_Op* tl_timelinePushSketch(tl_Timeline* tl, HMM_Vec2 pos, sk_Sketch sketch) {
 }
 
 tl_Op* tl_timelinePushBaseGeometry(tl_Timeline* tl, HMM_Vec2 pos, geo_Mesh mesh) {
-    tl_Op* out = SNZ_ARENA_PUSH(tl->arena, tl_Op);
+    tl_Op* out = SNZ_ARENA_PUSH(tl->operationArena, tl_Op);
     *out = (tl_Op){
         .ui.pos = pos,
         .kind = TL_OPK_BASE_GEOMETRY,
@@ -116,26 +121,6 @@ void tl_timelineDeselectAll(tl_Timeline* tl) {
     }
 }
 
-// FIXME: pool so that these get reused at some point instead of leaked
-void tl_timelineClearOpsMarkedForDelete(tl_Timeline* timeline) {
-    tl_Op* prev = NULL;
-    for (tl_Op* op = timeline->firstOp; op; op = op->next) {
-        if (op->markedForDeletion) {
-            if (prev) {
-                prev->next = op->next;
-            } else {
-                timeline->firstOp = op->next;
-            }
-
-            if (op == timeline->activeOp) {
-                timeline->activeOp = NULL;
-            }
-        } else {
-            prev = op;
-        }
-    }
-}
-
 static bool tl_timelineAnySelected(tl_Timeline* tl) {
     for (tl_Op* o = tl->firstOp; o; o = o->next) {
         if (o->ui.sel.selected) {
@@ -145,30 +130,84 @@ static bool tl_timelineAnySelected(tl_Timeline* tl) {
     return false;
 }
 
-void tl_solve(tl_Timeline* t, snz_Arena* arena, snz_Arena* scratch, PoolAlloc* pool) {
-    for (tl_Op* op = t->firstOp; op; op = op->next) {
-        op->solved = false;
-        if (op->kind == TL_OPK_BASE_GEOMETRY) {
-            op->solved = true;
+void tl_solve(tl_Timeline* t, snz_Arena* scratch) {
+    while (true) { // FIXME: emergency cutoff
+        bool anyChanged = false;
+        for (tl_Op* op = t->firstOp; op; op = op->next) {
+            if (op->status == TL_OPS_MARKED_FOR_DELETE) {
+                continue;
+            }
+
+            tl_Op* dep = op->dependencies[0];
+            if (dep) {
+                if (dep->status == TL_OPS_MARKED_FOR_DELETE) {
+                    op->dependencies[0] = NULL;
+                } else if (dep->status == TL_OPS_INVALIDATED) {
+                    op->status = TL_OPS_INVALIDATED;
+                }
+                anyChanged = true;
+            } // end dep check
+        }
+
+        if (anyChanged) {
+            break;
         }
     }
 
-    for (tl_Op* op = t->firstOp; op; op = op->next) {
-        if (op->kind == TL_OPK_SKETCH) {
-            // FIXME: bool status
-            sk_sketchSolve(&op->val.sketch.sketch);
-        } else if (op->kind == TL_OPK_SKETCH_GEOMETRY) {
-            tl_Op* other = op->val.sketchGeometry.sketch;
-            SNZ_ASSERTF(other->kind == TL_OPK_SKETCH, "dependent op wasn't expected kind. Was: %d, expected %d.", other->kind, TL_OPK_SKETCH);
-            SNZ_ASSERT(op->val.sketchGeometry.sketch, "dependent was null.");
-            geo_Mesh* m = SNZ_ARENA_PUSH(arena, geo_Mesh);
-            *m = skt_sketchTriangulate(&other->val.sketch.sketch, arena, scratch, pool);
-            other->scene.mesh = m;
+    { // cull elts marked for delete
+        tl_Op* newFirst = NULL;
+        tl_Op* next = NULL;
+        for (tl_Op* op = t->firstOp; op; op = next) {
+            next = op->next;
+            if (op->status == TL_OPS_MARKED_FOR_DELETE) {
+                continue;
+            }
+            op->next = newFirst;
+            newFirst = op;
         }
-        op->solved = true;
-    } // end solving
-
-    for (tl_Op* op = t->firstOp; op; op = op->next) {
-        SNZ_ASSERT(op->solved, "unsolved tl op.");
+        t->firstOp = newFirst;
     }
+
+    // collect render meshes
+    for (tl_Op* op = t->firstOp; op; op = op->next) {
+        if (op->status != TL_OPS_INVALIDATED) {
+            continue;
+        }
+
+        if (op->scene.mesh) {
+            ren3d_meshDeinit(&op->scene.mesh->renderMesh);
+        }
+        op->scene.mesh = NULL;
+    }
+
+    // FIXME: free arenas and pools with generated data
+
+    // resolve
+    while (true) { // FIXME: emergency cutoff
+        bool anySolved = false;
+        for (tl_Op* op = t->firstOp; op; op = op->next) {
+            SNZ_ASSERT(op->status != TL_OPS_MARKED_FOR_DELETE, "tl op marked for delete made it to the solving stage");
+            if (!op->status != TL_OPS_INVALIDATED) {
+                continue;
+            } else if (op->dependencies[0] && op->dependencies[0]->status != TL_OPS_SOLVED) {
+                continue;
+            }
+
+            if (op->kind == TL_OPK_SKETCH) {
+                sk_sketchSolve(&op->val.sketch.sketch);
+                op->status = TL_OPS_SOLVED;
+            } else if (op->kind == TL_OPK_SKETCH_GEOMETRY) {
+                tl_Op* other = op->val.sketchGeometry.sketch;
+                SNZ_ASSERTF(other->kind == TL_OPK_SKETCH, "dependent op wasn't expected kind. Was: %d, expected %d.", other->kind, TL_OPK_SKETCH);
+                SNZ_ASSERT(op->val.sketchGeometry.sketch, "dependent was null.");
+
+                geo_Mesh* m = SNZ_ARENA_PUSH(t->generatedArena, geo_Mesh);
+                *m = skt_sketchTriangulate(&other->val.sketch.sketch, t->generatedArena, scratch);
+                m->renderMesh = geo_BSPTriListToRenderMesh(m->bspTris, scratch);
+                geo_BSPTriListToFaceTris(t->generatedPool, m);
+                other->scene.mesh = m;
+                op->status = TL_OPS_SOLVED;
+            }
+        }
+    } // end loop to iteratvely solve
 }
