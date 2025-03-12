4a0d49da803aa38364c4bfa5a8133c3e6957f295
robbie aldrich <robbiemaldrich@gmail.com>
2025-03-12 18:40:59 -0500

    spec construction for ptr views working

--- src/ser.h
+++ src/ser.h
@@ -1,4 +1,5 @@
 #include "snooze.h"
+#include "settings.h"
 /*
 
 open qs:
@@ -32,7 +33,7 @@ typedef enum {
 
     SER_TK_STRUCT,
     SER_TK_PTR,
-    SER_TK_ARRAY,
+    SER_TK_PTR_VIEW,
 
     SER_TK_INT8,
     SER_TK_INT16,
@@ -53,7 +54,7 @@ const char* ser_tKindStrs[] = {
     "SER_TK_INVALID",
     "SER_TK_STRUCT",
     "SER_TK_PTR",
-    "SER_TK_ARRAY",
+    "SER_TK_PTR_VIEW",
 
     "SER_TK_INT8",
     "SER_TK_INT16",
@@ -74,8 +75,10 @@ typedef struct ser_SpecStruct ser_SpecStruct;
 struct ser_T {
     ser_TKind kind;
     ser_T* inner;
+
     const char* referencedName;
     ser_SpecStruct* referencedStruct;
+    int offsetOfPtrViewLengthIntoStruct;
 };
 
 struct ser_SpecField {
@@ -127,7 +130,7 @@ ser_T* ser_tBase(ser_TKind kind) {
     SNZ_ASSERT(kind != SER_TK_INVALID, "Kind of 0 (SER_TK_INVALID) isn't a base kind.");
     SNZ_ASSERT(kind != SER_TK_STRUCT, "Kind of 1 (SER_TK_STRUCT) isn't a base kind.");
     SNZ_ASSERT(kind != SER_TK_PTR, "Kind of 2 (SER_TK_PTR) isn't a base kind.");
-    SNZ_ASSERT(kind != SER_TK_ARRAY, "Kind of 3 (SER_TK_ARRAY) isn't a base kind.");
+    SNZ_ASSERT(kind != SER_TK_PTR_VIEW, "Kind of 3 (SER_TK_PTR_VIEW) isn't a base kind.");
     ser_T* out = SNZ_ARENA_PUSH(_ser_globs.arena, ser_T);
     out->kind = kind;
     return out;
@@ -151,15 +154,7 @@ ser_T* ser_tPtr(ser_T* innerT) {
     return out;
 }
 
-ser_T* ser_tArray(ser_T* innerT) {
-    _ser_assertInstanceValidForAddingToSpec();
-    ser_T* out = SNZ_ARENA_PUSH(_ser_globs.arena, ser_T);
-    out->kind = SER_TK_ARRAY;
-    SNZ_ASSERT(innerT, "Can't point to a null type.");
-    out->inner = innerT;
-    return out;
-}
-
+// FIXME: add a sizeof in this to typecheck that T is a valid symbol but also for arr deser
 #define ser_addStruct(T) _ser_addStruct(#T)
 void _ser_addStruct(const char* name) {
     _ser_assertInstanceValidForAddingToSpec();
@@ -171,8 +166,8 @@ void _ser_addStruct(const char* name) {
 
 // FIXME: assert that active struct name == struct name?
 // FIXME: assert that size of kind is same as sizeof prop named
-#define ser_addStructField(kindPtr, structName, name) \
-    _ser_addStructField(kindPtr, #name, offsetof(structName, name))
+#define ser_addStructField(kindPtr, structT, name) \
+    _ser_addStructField(kindPtr, #name, offsetof(structT, name))
 void _ser_addStructField(ser_T* kind, const char* tag, int offsetIntoStruct) {
     _ser_assertInstanceValidForAddingToSpec();
     SNZ_ASSERT(_ser_globs.activeStructSpec, "There was no active struct to add a field to.");
@@ -186,6 +181,23 @@ void _ser_addStructField(ser_T* kind, const char* tag, int offsetIntoStruct) {
     };
     _ser_globs.activeStructSpec->firstField = field;
     _ser_globs.activeStructSpec->fieldCount++;
+}
+
+// FIXME: switch on type and or assert that len prop is an int64_t
+#define ser_addStructFieldPtrView(innerKind, structT, ptrPropName, lenPropName) \
+    _ser_addStructFieldPtrView(innerKind, #ptrPropName, offsetof(structT, ptrPropName), offsetof(structT, lenPropName))
+void _ser_addStructFieldPtrView(ser_T* innerKind, const char* ptrPropTag, int ptrPropOffset, int lenPropOffset) {
+    _ser_assertInstanceValidForAddingToSpec();
+
+    SNZ_ASSERT(innerKind->kind != SER_TK_PTR_VIEW, "can't nest ptr views cause the inner length woudn't exist anywhere. Wrap it in a struct.");
+    ser_T* viewT = SNZ_ARENA_PUSH(_ser_globs.arena, ser_T);
+    *viewT = (ser_T){
+        .inner = innerKind,
+        .kind = SER_TK_PTR_VIEW,
+        .offsetOfPtrViewLengthIntoStruct = lenPropOffset,
+    };
+
+    _ser_addStructField(viewT, ptrPropTag, ptrPropOffset);
 }
 
 void ser_begin(snz_Arena* arena) {
@@ -203,10 +215,9 @@ ser_SpecStruct* _ser_getStructSpecByName(const char* name) {
 }
 
 bool _ser_kindNonTerminal(ser_TKind kind) {
-    SNZ_ASSERTF(kind > SER_TK_INVALID || kind < SER_TK_COUNT, "invalid kind '%d'", kind);
     if (kind == SER_TK_PTR) {
         return true;
-    } else if (kind == SER_TK_ARRAY) {
+    } else if (kind == SER_TK_PTR_VIEW) {
         return true;
     }
     return false;
@@ -224,14 +235,22 @@ void ser_end() {
         SNZ_ASSERT(s->tag, "struct with no tag.");
         for (ser_SpecField* f = s->firstField; f; f = f->next) {
             SNZ_ASSERT(f->tag, "struct field with no tag.");
+            SNZ_ASSERT(f->type, "can't have a field with no type.");
+
             for (ser_T* inner = f->type; inner; inner = inner->inner) {
+                SNZ_ASSERTF(inner->kind > SER_TK_INVALID && inner->kind < SER_TK_COUNT, "invalid kind: %d.", inner->kind);
                 if (inner->kind == SER_TK_STRUCT) {
                     inner->referencedStruct = _ser_getStructSpecByName(inner->referencedName);
                     SNZ_ASSERTF(inner->referencedStruct, "no definition with the name '%s' found.", inner->referencedName);
-                }
 
-                if (_ser_kindNonTerminal(inner->kind)) {
-                    SNZ_ASSERT(inner->inner, "non-terminal kind with no inner kind");
+                    if (inner == f->type) {
+                        SNZ_ASSERT(inner->referencedStruct != s, "can't nest structs. make the field a ptr or ptr view.");
+                    }
+                } else if (inner->kind == SER_TK_PTR) {
+                    SNZ_ASSERT(inner->inner, "ptr with no inner kind");
+                } else if (inner->kind == SER_TK_PTR_VIEW) {
+                    SNZ_ASSERT(inner->inner, "ptr with no inner kind");
+                    SNZ_ASSERT(inner->inner->kind != SER_TK_PTR_VIEW, "nested ptr view.");
                 } else {
                     SNZ_ASSERT(!inner->inner, "terminal kind with an inner kind");
                 }
@@ -258,11 +277,11 @@ void _ser_printSpec() {
 }
 
 typedef struct _ser_QueuedStruct _ser_QueuedStruct;
-typedef struct {
+struct _ser_QueuedStruct {
     _ser_QueuedStruct* next;
     ser_SpecStruct* spec;
     void* obj;
-} _ser_QueuedStruct;
+};
 
 typedef struct {
     _ser_QueuedStruct* nextStruct;
@@ -272,27 +291,30 @@ typedef struct {
 } _ser_WriteInst;
 
 void _ser_writeField(_ser_WriteInst* write, ser_SpecField* field, void* obj, int indentLevel) {
+    fprintf(write->file, "%*s", indentLevel * 4, "");
+
     ser_TKind kind = field->type->kind;
     if (kind == SER_TK_PTR) {
+        fprintf(write->file, ">> ptr\n");
         // FIXME: dedup deps
-        fprintf(write->file, "no idea what to do w this one tbh.\n");
         return;
-    } else if (kind == SER_TK_ARRAY) {
-        fprintf(write->file, "no idea what to do w this one tbh.\n");
+    } else if (kind == SER_TK_PTR_VIEW) {
+        fprintf(write->file, ">> slice, ");
+        int64_t len = *(int64_t*)((char*)obj + field->type->offsetOfPtrViewLengthIntoStruct);
+        void* innerPtr = *(void**)((char*)obj + field->offsetInStruct);
+        fprintf(write->file, "%lld elems @ %p\n", len, innerPtr);
+        // FIXME: ptr patching
         return;
     } else if (kind == SER_TK_STRUCT) {
+        fprintf(write->file, ">> struct\n");
         ser_SpecStruct* s = field->type->referencedStruct;
         void* innerStruct = (char*)obj + field->offsetInStruct;
         for (ser_SpecField* innerField = s->firstField; innerField; innerField = innerField->next) {
             _ser_writeField(write, innerField, innerStruct, indentLevel + 1);
         }
         return;
-    } else {
-        SNZ_ASSERTF(false, "unreachable. kind: %d.", kind);
     }
 
-    // base types
-    fprintf(write->file, "%*s", indentLevel * 2, "");
 
     void* ptr = ((char*)obj) + field->offsetInStruct;
     if (kind == SER_TK_FLOAT32) {
@@ -315,10 +337,13 @@ void _ser_writeField(_ser_WriteInst* write, ser_SpecField* field, void* obj, int
         fprintf(write->file, "%u", *(uint32_t*)ptr);
     } else if (kind == SER_TK_UINT64) {
         fprintf(write->file, "%llu", *(uint64_t*)ptr);
+    } else {
+        SNZ_ASSERTF(false, "unreachable. kind: %d.", kind);
     }
     fprintf(write->file, "\n");
 }
 
+// FIXME: typecheck of some kind on obj
 #define ser_write(F, T, obj, scratch) _ser_write(F, #T, obj, scratch)
 void _ser_write(FILE* f, const char* typename, void* obj, snz_Arena* scratch) {
     _ser_assertInstanceValidated();
@@ -362,6 +387,7 @@ void _ser_write(FILE* f, const char* typename, void* obj, snz_Arena* scratch) {
     }
 }
 
+#include "timeline.h"
 #include "geometry.h"
 
 void ser_tests() {
@@ -384,24 +410,50 @@ void ser_tests() {
     ser_addStructField(ser_tStruct(HMM_Vec3), geo_Tri, c);
 
     ser_addStruct(geo_TriSlice);
-    ser_addStructField(ser_tArray(ser_tStruct(geo_Tri)), geo_TriSlice, elems);
+    ser_addStructFieldPtrView(ser_tStruct(geo_Tri), geo_TriSlice, elems, count);
 
     ser_addStruct(geo_Line);
     ser_addStructField(ser_tStruct(HMM_Vec3), geo_Line, a);
     ser_addStructField(ser_tStruct(HMM_Vec3), geo_Line, b);
 
     ser_addStruct(geo_LineSlice);
-    ser_addStructField(ser_tArray(ser_tStruct(geo_Line)), geo_LineSlice, elems);
+    ser_addStructFieldPtrView(ser_tStruct(geo_Line), geo_LineSlice, elems, count);
+
+    ser_addStruct(set_Settings);
+    ser_addStructField(ser_tBase(SER_TK_INT8), set_Settings, darkMode);
+    ser_addStructField(ser_tBase(SER_TK_INT8), set_Settings, musicMode);
+    ser_addStructField(ser_tBase(SER_TK_INT8), set_Settings, skybox);
+    ser_addStructField(ser_tBase(SER_TK_INT8), set_Settings, hintWindowAlwaysOpen);
+    ser_addStructField(ser_tBase(SER_TK_INT8), set_Settings, leftBarAlwaysOpen);
+    ser_addStructField(ser_tBase(SER_TK_INT8), set_Settings, timelinePreviewSpinBackground);
+    ser_addStructField(ser_tBase(SER_TK_INT8), set_Settings, squishyCamera);
+    ser_addStructField(ser_tBase(SER_TK_INT8), set_Settings, crosshair);
+
+    ser_addStruct(tl_Op);
+    ser_addStructField(ser_tPtr(ser_tStruct(tl_Op)), tl_Op, next);
+    ser_addStructField(ser_tBase(SER_TK_INT32), tl_Op, kind);
+    ser_addStructField(ser_tPtr(ser_tStruct(tl_Op)), tl_Op, dependencies[0]);
 
     ser_end();
 
     FILE* f = fopen("testing/ser3.adder", "w");
-    geo_Tri t = (geo_Tri){
-        .a = HMM_V3(0, 1, 2),
-        .b = HMM_V3(3, 4, 5),
-        .c = HMM_V3(6, 7, 8),
+    geo_Tri tris[] = {
+        (geo_Tri) {
+            .a = HMM_V3(0, 1, 2),
+            .b = HMM_V3(3, 4, 5),
+            .c = HMM_V3(6, 7, 8),
+        },
+        (geo_Tri) {
+            .a = HMM_V3(00, 10, 20),
+            .b = HMM_V3(30, 40, 50),
+            .c = HMM_V3(60, 70, 80),
+        },
     };
-    ser_write(f, geo_Tri, &t, &testArena);
+    geo_TriSlice slice = (geo_TriSlice){
+        .elems = tris,
+        .count = sizeof(tris) / sizeof(*tris),
+    };
+    ser_write(f, geo_TriSlice, &slice, &testArena);
     // _ser_printSpec();
     fclose(f);
 }
